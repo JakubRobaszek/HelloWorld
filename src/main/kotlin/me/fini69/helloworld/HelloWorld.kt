f938b3c5546ac857c05a2e79f515631c2f16f4bf
Jakub Robaszek <112584909+JakubRobaszek@users.noreply.github.com>
2024-10-29 20:35:03 +0100

    Created a Hello command and made fixes

--- src/main/kotlin/me/fini69/helloworld/HelloWorld.kt
+++ src/main/kotlin/me/fini69/helloworld/HelloWorld.kt
@@ -1,14 +1,33 @@
 package me.fini69.helloworld
 
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class HelloWorld : JavaPlugin() {
 
     override fun onEnable() {
+        // Log a message to the console when the plugin is enabled
         logger.info("Hello from Paper! Plugin has been enabled!")
     }
 
     override fun onDisable() {
+        // Log a message to the console when the plugin is disabled
         logger.info("Hello from Paper! Plugin has been disabled!")
+    }
+
+    // Command handling
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (sender is Player) {
+            when(command.name) {
+                "hello" -> {
+                    if(args.isEmpty()) {
+                        sender.sendMessage("Hello ${sender.name}!")
+                    }
+                }
+            }
+        }
+        return true
     }
 }
